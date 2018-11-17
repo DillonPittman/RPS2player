aa2b02b9b0213eb38fd32bccecf01aa85a78cbd0
DillonPittman <pittmanwdillon@gmail.com>
2018-11-17 09:56:05 -0600

    html with bootstrap done, need to code the game with firebase.

---
+++ assets/config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDOEoLQfEZ5IRVqY2nS2RQcfOpnXCgBsWE",
+    authDomain: "rps-2-player-5ff61.firebaseapp.com",
+    databaseURL: "https://rps-2-player-5ff61.firebaseio.com",
+    projectId: "rps-2-player-5ff61",
+    storageBucket: "rps-2-player-5ff61.appspot.com",
+    messagingSenderId: "406227875169"
+  };
+  firebase.initializeApp(config);
